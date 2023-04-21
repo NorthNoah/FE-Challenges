78c3cac9126389a4bd65d63715594b8751474db6
NorthNoah <1264918006@qq.com>
2023-04-21 20:26:58 +0800

    feat:函数柯里化

--- JS手写题/09函数柯里化.js
+++ JS手写题/09函数柯里化.js
@@ -1,9 +1,23 @@
+// 柯里化的定义：接收一部分参数，返回一个函数接收剩余参数，接收足够参数后，执行原函数。
 // ES6写法
 function curry(fn) {
-    let judge = (...args) => {
-        // 若本次执行的实际参数和形参数量相等，则直接执行fn即可
-        if (args.length === fn.length) return fn(...args)
-        return (...arg) => judge(...args, ...arg)
+    return function temp(...args) {
+        if (fn.length === args.length) {
+            return fn(...args)
+        } else {
+            return function(...arg) {
+                return temp(...arg, ...args)
+            }
+        }
     }
-    return judge
 }
+
+function add(a, b, c) {
+    return a + b + c
+}
+
+let curryAdd = curry(add)
+let a = curryAdd(1, 2, 3)
+let b = curryAdd(1)(2, 3)
+let c = curryAdd(1)(2)(3)
+console.log(a, b, c)
