80e185c3edee2afc06345e90c42639d2e105a0c1
NorthNoah <1264918006@qq.com>
2023-04-28 19:58:55 +0800

    feat:数组去重

--- JS手写题/12数组问题/01数组去重.js
+++ JS手写题/12数组问题/01数组去重.js
@@ -1,6 +1,6 @@
 const arr = [1,2,3,4,4,4]
 
-// Map + filter：使用map筛选
+// filter + map：使用map标记元素，filter进行筛选
 const unique = arr => {
     const map = new Map()
     // 若不存在key（item），则不计入筛选；若
@@ -10,7 +10,37 @@ const unique = arr => {
 
 // filter + indexOf:寻找第一次出现的index，与实际index进行比较
 const unique2 = arr => {
-    const map = 
+    const res = arr.filter((item, index) => arr.indexOf(item) === index) 
+    return res
 }
+
+
+// includes + map：新建数组[],不含则push
+const unique3 = arr => {
+    let res = []
+    arr.map(item => {
+        // 当结果数组中不含有当前元素，则push进入数组
+        if (!res.includes(item)) {
+            res.push(item)
+        }
+    })
+    return res
+}
+
+// set直接去重
+const unique4 = arr => {
+    const res = Array.from(new Set(arr))
+    return res
+}
+
+console.log(unique(arr))
+console.log(unique2(arr))
+console.log(unique3(arr))
+console.log(unique4(arr))
+
+
+
+
+
 
 
