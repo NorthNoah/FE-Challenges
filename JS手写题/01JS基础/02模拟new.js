199de47e61ec8805dddb52d6fc8f756e2e19d221
NorthNoah <1264918006@qq.com>
2023-05-01 17:34:40 +0800

    chore：重构目录

--- JS手写题/01JS基础/02模拟new.js
+++ JS手写题/01JS基础/02模拟new.js
@@ -12,12 +12,3 @@ function myNew(fn, ...args) {
     return typeof instance === 'object' ? res : instance
 }
 
-// 模拟Object.create：将构造函数的prototype指向指定proto，然后new Fn()自动绑定
-function ObjectCreate(proto) {
-    function Fn() {}
-    Fn.prototype = proto
-    // 自动绑定newObj = Fn.prototype = proto
-    return new Fn()
-}
-
-
