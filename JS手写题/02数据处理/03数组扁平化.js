7950cc7b37854c7b4bec0249e212a4e6011cfa12
NorthNoah <1264918006@qq.com>
2023-05-02 17:33:02 +0800

    feat：数组扁平化递归解法

--- JS手写题/02数据处理/03数组扁平化.js
+++ JS手写题/02数据处理/03数组扁平化.js
@@ -8,8 +8,8 @@ const flat2 = (arr) => {
     const res = 
 	arr.toString().split(",").map(item => +item)
     return res
-    
 }
+
 // 3.reduce + concat
 const flat3 = arr => {
     // pre：累积和，cur：当前元素
@@ -18,6 +18,22 @@ const flat3 = arr => {
     }, [])
 }
 
+// 4.递归实现扁平化
+const flatten = arr => {
+    let res = []
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            // 仍存在未扁平的数组，递归后拼接
+            res = res.concat(flatten(arr[i]))
+        } else {
+            // 已经扁平，直接push进res
+            res.push(arr[i])
+        }
+    }
+    return res
+}
+
 console.log(flat1(arr))
 console.log(flat2(arr))
 console.log(flat3(arr))
+console.log(flatten(arr))
