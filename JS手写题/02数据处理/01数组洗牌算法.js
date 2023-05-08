75d692449c83b330b967074659e997948a04780b
NorthNoah <1264918006@qq.com>
2023-05-08 15:44:35 +0800

    Update：数组洗牌算法

--- JS手写题/02数据处理/01数组洗牌算法.js
+++ JS手写题/02数据处理/01数组洗牌算法.js
@@ -2,8 +2,8 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // 当前数字
 function shuffle1(arr) {
 	for (let i = 0; i < arr.length; i++) {
-		const randomIndex = Math.round(Math.random() * (arr.length - 1 - i)) + i
-		;[arr[i], arr[randomIndex]] = [arr[randomIndex], arr[i]]
+		const randomIndex = Math.round(Math.random() * (arr.length - 1 - i)) + i;
+		[arr[i], arr[randomIndex]] = [arr[randomIndex], arr[i]]
 	}
 	return arr
 }
