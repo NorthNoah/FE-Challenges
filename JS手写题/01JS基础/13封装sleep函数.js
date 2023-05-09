14ccc318e0a18f2144a9e066f398463a73b20da0
NorthNoah <1264918006@qq.com>
2023-05-09 11:07:25 +0800

    Create 13封装sleep函数.js

---
+++ JS手写题/01JS基础/13封装sleep函数.js
@@ -0,0 +1,38 @@
+// 1.直接使用promise+then调用，可以链式调用，此时为异步任务无法阻塞同步任务
+function sleep(time) {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve()
+		}, time)
+	})
+}
+
+function fnA() {
+	console.log("A")
+}
+function fnB() {
+	console.log("B")
+}
+function fnC() {
+	console.log("C")
+}
+
+// sleep(1000).then(fnA)
+// console.log("E")
+// sleep(2000).then(fnB)
+// console.log("G")
+// sleep(3000).then(fnC)
+
+// 2.async+await调用来阻塞代码，依次执行
+async function sleepTest() {
+    fnA()
+    // 返回结果才会继续调用同步代码
+	await sleep(1000)
+	console.log("E")
+    fnB()
+	await sleep(1000)
+    fnC()
+	await sleep(1000)
+	console.log("G")
+}
+sleepTest()
