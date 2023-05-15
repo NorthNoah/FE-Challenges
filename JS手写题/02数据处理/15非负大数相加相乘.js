3d79b5e9e30528814955a52efc1efb5f4c2453d0
NorthNoah <1264918006@qq.com>
2023-05-15 10:02:15 +0800

    Update 15非负大数相加相乘.js

--- JS手写题/02数据处理/15非负大数相加相乘.js
+++ JS手写题/02数据处理/15非负大数相加相乘.js
@@ -1,21 +1,50 @@
 // 大数相加：字符串保存大数，转化为数组计算；将当前位的结果对10进行取余操作，保存结果；
 function sumBigNumber(a, b) {
-    let res = ''
-    let temp = 0
-    
-    // 转为数组
-    a = a.toString().split('')
-    b = b.toString().split('')
+    a = a + ""
+    b = b + ""
+    // 找最大的length，头部补零
+    let len = Math.max(a.length, b.length)
+    a = a.padStart(len, '0')
+    b = b.padStart(len, '0')
 
-    // while循环，对每一位进行相加
-    while (a.length || b.length || temp) {
-        // console.log(~~a.pop())
-        temp += ~~a.pop() + ~~b.pop()
-        // 保存当前结果
-        res = (temp % 10) + res
-        // 判断是否进位
-        temp = temp > 9
+    let res = []
+    // 倒序按位相加
+    for (let i = len - 1; i >= 0; i--) {
+        // 相加进位,注意此时可能有进位
+        let sum = Number(a[i]) + Number(b[i]) + (res[0] || 0)
+        let low = sum % 10 //本位的计算结果
+        let high = Math.floor(sum / 10)//进位数字
+
+        res[0] = low
+        res.unshift(high)
     }
-    return res
+    return +res.join('')
 }
-console.log(sumBigNumber(111111111111112222222222222,1111111239999999))
+console.log(sumBigNumber(22,25))
+console.log(sumBigNumber(31,33111))
+
+
+// 大数相乘
+function multiplyBigNum(a, b) {
+    a = a.toString()
+    b = b.toString()
+
+    let len1 = a.length
+    let len2 = b.length
+    let res = []
+
+    for (let j = len2 - 1; j >= 0; j--) {
+        for (let i = len1 - 1; i >= 0; i--) {
+            // 十位
+            let index1 = i + j
+            // 个位
+            let index2 = i + j + 1
+            // index2：现在的个位，之前的十位
+            let mult = a[i] * b[j] + (res[index2] || 0)
+            res[index1] = Math.floor(mult / 10)
+            res[index2] = mult % 10
+        }
+    }
+    return +res.join("")
+}
+console.log(multiplyBigNum(33, 22))
